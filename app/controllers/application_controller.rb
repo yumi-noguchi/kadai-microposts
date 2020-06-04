7c79cc4b754f81d52ae04b0995e0cac24c80b945
yumi.noguchi <anzu1027@gmail.com>
2020-06-04 11:27:19 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,7 @@ class ApplicationController < ActionController::Base
   end
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(10)
   end
