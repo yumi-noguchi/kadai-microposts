a947f8037b25496719ef7f5f45a4d9a68678cfae
yumi.noguchi <anzu1027@gmail.com>
2020-07-20 08:58:45 +0000

    0720

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,14 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    @micropost = Micropost.find(params[:id])
+    favorite = current_user.like.create(micropost_id: params[:micropost_id])
     @micropost.like(current_user)
     flash[:success] = "この投稿をお気に入りに登録しました"
     redirect_to user
   end
 
   def destroy
-    @micropost = Micropost.find(params[:micropost_id])
+    @micropost = Micropost.find(micropost_id: params[:micropost_id])
     @micropost.unlike(current_user)
     flash[:danger] = "この投稿のお気に入りを解除しました"
     redirect_to user
